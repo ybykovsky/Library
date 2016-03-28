f123306bcae1fdc10993ea0224bcb39cc7042c39
Yury Bykovsky <yury.bykovsky@wsa-127.icx.local>
2016-03-28 13:55:30 +0300

    Web Api

---
+++ Library.Web/Controllers/BookController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Library.Domain.Interfaces;
+
+namespace Library.Web.Controllers
+{
+    public class BookController : ApiController
+    {
+        private IBookRepository _bookRepository;
+
+        public BookController(IBookRepository bookRepository)
+        {
+            _bookRepository = bookRepository;
+        }
+
+        public void Reserve(Guid userId, Guid bookId)
+        { 
+
+        }
+    }
+}
